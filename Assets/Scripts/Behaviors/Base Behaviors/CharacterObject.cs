cc9561a2958cc1f639563474a23fed567ef25da6
AndreasJensen <44391388+YungSav4ge@users.noreply.github.com>
2021-08-27 13:47:29 +0200

    dash

--- Assets/Scripts/Behaviors/Base Behaviors/CharacterObject.cs
+++ Assets/Scripts/Behaviors/Base Behaviors/CharacterObject.cs
@@ -18,24 +18,60 @@ public class CharacterObject : MonoBehaviour
         }
     }
 
-    void LoseHealth(float damageTaken) {
+    public void LoseHealth(float damageTaken) {
         //float damageResult = maxHP - damageTaken;
         currentHP -= damageTaken;
 
         if(isDead) {
-            die();
+            Die();
         }
     }
 
-    private void die() {
+    private void Die() {
         throw new NotImplementedException();
 
     }
 
-    void RecoverHealth(float healthRecovered) {
+    public void RecoverHealth(float healthRecovered) {
         currentHP += healthRecovered;
         if(currentHP > maxHP) {
             currentHP = maxHP;
         }
+    }
+
+    /// <summary>
+    /// Regaining your health overtime
+    /// </summary>
+    /// <param name="totalAmount">How much you will heal in total</param>
+    /// <param name="totalDuration">How long it will take/be active</param>
+    /// <param name="totalTicks">how many times you will get health</param>
+    /// <returns></returns>
+    public IEnumerator RecoverHealthOvertime(float totalAmount, float totalDuration, int totalTicks){
+        //timeUntilHeal is to calculate how long it will take to gaining the total health
+        float timeUntilHeal = totalDuration / totalTicks;
+        for (int i = 0; i < totalTicks; i++){
+            yield return new WaitForSeconds(timeUntilHeal);
+
+            RecoverHealth(totalAmount / totalTicks);
+        }
+    }
+
+    /// <summary>
+    /// Loseing your health from effects overtime
+    /// </summary>
+    /// <param name="totalAmount">How much dmg you will lose in total</param>
+    /// <param name="totalDuration">How long it will take/be active</param>
+    /// <param name="totalTicks">how many times you will take dmg</param>
+    /// <returns></returns>
+    public IEnumerator LoseHealthOvertime(float totalAmount, float totalDuration, int totalTicks){
+        //timeUntilDamaged is to calculate how long will take to take the total dmg
+        float timeUntilDamaged = totalDuration / totalTicks;
+        for (int i = 0; i < totalTicks; i++) {
+            yield return new WaitForSeconds(timeUntilDamaged);
+
+            LoseHealth(totalAmount / totalTicks);
+        }
+        
+
     }
 }
