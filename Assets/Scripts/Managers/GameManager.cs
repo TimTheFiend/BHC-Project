465c7cc4749667547d3319098fa584e4b2d55e5d
AndreasJensen <44391388+YungSav4ge@users.noreply.github.com>
2021-09-21 11:36:32 +0200

    weight drop

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -7,7 +8,6 @@ public class GameManager : MonoBehaviour
     public static GameManager instance = null; // Singleton
 
     public GameObject player;
-
 
     private void Awake() {
         #region Singleton Pattern
@@ -19,6 +19,22 @@ public class GameManager : MonoBehaviour
         }
         DontDestroyOnLoad(gameObject);
         #endregion
+    }
+
+    /// <summary>
+    /// Laver et List view og vælger en random værdi som skal "droppes" inden for en vis værdi
+    /// </summary>
+    /// <param name="_weightDict"></param>
+    public void TempWeight(Dictionary<string, float> _weightDict) {
+        float value = Random.value;
+
+        List<string> keys = new List<string>();
+        keys.AddRange(_weightDict.Where(w => w.Value >= value).Select(w => w.Key).ToList());
+
+        if (keys.Count == 0) {
+            return;
+        }
+        print($"{value} - {keys[Random.Range(0, keys.Count)]}");
     }
 
 
