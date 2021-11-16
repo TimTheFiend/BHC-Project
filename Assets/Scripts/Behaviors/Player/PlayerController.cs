6180c151146249bbbaaec5c280979807ff25a679
AndreasJensen <andreasjensen2300@gmail.com>
2021-11-16 11:51:41 +0100

    UI HP Bar

--- Assets/Scripts/Behaviors/Player/PlayerController.cs
+++ Assets/Scripts/Behaviors/Player/PlayerController.cs
@@ -18,6 +18,7 @@ public class PlayerController : CharacterObject
 
         //TODO fix
         currentHP = maxHP;
+
     }
 
     public void OnMovement(InputAction.CallbackContext _context) {
@@ -33,4 +34,11 @@ public class PlayerController : CharacterObject
     public void OnDash(InputAction.CallbackContext _context) {
         pMovement.AttemptDash();
     }
+
+    public override void LoseHealth(float healthLost) {
+        base.LoseHealth(healthLost);
+        UIManager.instance.UpdateHPBar = currentHP / maxHP * 100;
+        Debug.Log(currentHP / maxHP * 100);
+    }
+    
 }
